bb70a3de6b250eece992d528400e3811589748f4
Samer <samerbenmim@insat.u-carthage.tn>
2021-07-02 12:48:52 +0100

    depoy on gh

--- src/Component/ErrorPage/ErrorPage.js
+++ src/Component/ErrorPage/ErrorPage.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react'
+import Navbar from '../Home/Navbar'
 
 export class ErrorPage extends Component {
     render() {
-        return (
+        return (<>
+                            <Navbar/>
+
             <div style={{textAlign:'center'}}>
                  <h1 style={{textAlign:"center"}}>Error Page</h1>
-            </div>
+            </div></>
         )
     }
 }
